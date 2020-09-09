147d37b01f6aff95cea64e37bf1809f1f619b5c1
Nikola Jovicic <nikolajovicic.nj@gmail.com>
2020-09-09 17:08:46 +0200

    Optimizacija za mobilne uredjaje

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,13 +3,14 @@
         <vs-col>
             <h1>We are sorry, but the following page is not found...</h1>
             <img src="/img/earth-mask.jpg" alt="Stay Safe">
-                <router-link to="/"><h5 class="py-4">Go back to home page</h5></router-link>
+            <router-link to="/"><h5 class="py-4">Go back to home page</h5></router-link>
         </vs-col>
     </vs-row>
 </template>
 
 <style scoped>
 img {
-    max-height: 350px;
+    width: 100%;
+    max-width: 350px;
 }
 </style>
